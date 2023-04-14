1f1b7d15d3c9fe064c7f3145d6fb12140789f274
vyslbyrx@gmail.com <yolunsonu12456@@>
2023-04-14 10:17:57 +0200

    ürün link

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,21 +27,93 @@ dropdownList.forEach(function(dropdown) {
 });
 
 
+//
 
+const menu1 = [
+    {
+        "title": "HANTECH Split Klimaanlage GWH 18000 BTU Modell Lomo",
+        "link": "https://hantech.eu/produkt/hantech-split-klimaanlage-gwh-18000-btu-modell-lomo/",
+        "img": "https://hantech.eu/wp-content/uploads/2017/09/w1200_hantech_gwh-300x300.jpg"
+    },
+    {
+        "title": "HANTECH Split Klimaanlage GWH 24000 BTU Modell Lomo",
+        "link": "https://hantech.eu/produkt/hantech-split-klimaanlage-gwh-24000-btu-modell-lomo/",
+        "img": "https://hantech.eu/wp-content/uploads/2017/09/w1200_hantech_gwh-300x300.jpg"
+    },
+    {
+        "title": "HANTECH Split Klimaanlage GWH 9000 BTU Modell Lomo",
+        "link": "https://hantech.eu/produkt/hantech-split-klimaanlage-gwh-9000-btu-modell-lomo/",
+        "img": "https://hantech.eu/wp-content/uploads/2017/09/w1200_hantech_gwh-300x300.jpg"
+    }
+];
+const menu2 = [];
+const menu3 = [];
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get("name");
 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYmE4ZmU4ZjBhN2FiMWVjNzg4Y2Q3ZWFjNDg4ZWM2NjQ3MjhmN2Y5OTAzNjI3M2MwNjEwNzczNzBlMzQ1MWRhNzE3MjU1MzUwNzNiNWNiMDYiLCJpYXQiOjE2ODE0Mjc5ODIuMDcwNCwibmJmIjoxNjgxNDI3OTgyLjA3MDQsImV4cCI6MTY4MTUxNDM4Mi4wNjg4LCJzdWIiOiIyMyIsInNjb3BlcyI6WyIqIl19.lJtx7h3LIN3tMPssMuWitJOhshyVgPs6PTIVnvyeHs9xAdcfTga4BNl9vuFn6in6YuIw0t17qygx-cJiWP_phbcDqtCunrde_DHDE10eAGNZB447wEqmaQ0jgwRNQXS57YffUETRovwdlQuL3BiUzZD9jlG804rNfHifg72BefXfkqm-MpkpYGakVAgNWAPXye0WKb8E3sQ63EvH1wUOiUBjNyNRZB2j2V79G7tJdHxfbKg8a1mmcj5ltZoiitQcXW-I99omcywSQzh4sMYSVYmsS_ayBb6sRLwlL9odURN_hXVgMK701i9N_MFRgGvRyzxFTHkTqXsRi-k4lR9X_YYJt_0I1cxVSyyYe4Qw-mtXqlwKG1s_6lgu-i7PPf46DN2EkWGaBUsl3Uncg8tRMxLXCozi8r1OKVO149h6abXvRdpxWz4YIBEmsFXJJuPXzjE78XA10bbvroTHBEY9gEBHQkZtrCEw4jfhB1jq1_ErBsDHau1qN3RWu_HzXbKOBqcu3_apjM1rxjr6_xJ8MWifha0nO-GJWdId5bFJxFN54YSSHCojvtpdqM6fA8JAEhaM613wMuDqiTofznymq9XBJBoo9gXNS_L-SbBta5GQKjgpoQyCSYdqJO7gysYJDOIoD6ZpFBki5CwnJK_DJJCjAr1Nm6MGJiLl-ptha4A");
-myHeaders.append("Cookie", "plentyID=eyJpdiI6IjNPNFI3TC96ZGtMWVVGMktmRklub0E9PSIsInZhbHVlIjoiNVlJTDllUDRNTk9TTWxpWTl0NU9YK1gvRWlJK2VOY1k3R3JRd3laY04veUgyTy80UjBERys3alRDTnc4dThIaiIsIm1hYyI6IjgyZjk4YWYyZjdlYWFhODdjZThhMWVjYzI4OGVkMTZjMmE2NzVmMmE0YmU3OGU1ZDk5ZjNkN2Q1NWFmZjE0NDEiLCJ0YWciOiIifQ%3D%3D");
+if(name === 'Wandklimageräte') {
+    menu1.map(menuItem => {
+        const item = `<a href="/product-detail.html?name=${menuItem.title.toLowerCase().replaceAll(' ','_')}" class="h-product">
+        <div class="h-product__img">
+          <img src="${menuItem.img}" alt="">
+        </div>
+        <h3>${menuItem.title}</h3>
+        <div class="h-product__price">
+          <span>€1300,00</span>
+          <span class="old-price">€1500,00</span>
+        </div>
+        <small>inkl. MwSt. zzgl. Versand</small>
+        <button type="button" class="h-button h-button__primary">Details</button>
+      </a>`;
 
-var formdata = new FormData();
+        document.querySelector('.h-products').innerHTML += item;
+    });
+    if(menu1.length < 1) {
+        document.querySelector('.h-products').innerHTML += '<p class="kein-produkte">Es wurden keine Produkte gefunden, die deiner Auswahl entsprechen.</p>'
+    }
+}
 
-var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow',
-};
+if(name === 'Deckenkassetten') {
+    console.log(menu2.length)
+    if(menu2.length === 0) {
+        document.querySelector('section .h-container').innerHTML += '<p class="kein-produkte">Es wurden keine Produkte gefunden, die deiner Auswahl entsprechen.</p>'
+    } else {
+        menu2.map(menuItem => {
+            const item = `<a href="/product-detail.html?name=${menuItem.title.toLowerCase().replaceAll(' ','_')}" class="h-product">
+        <div class="h-product__img">
+          <img src="${menuItem.img}" alt="">
+        </div>
+        <h3>${menuItem.title}</h3>
+        <div class="h-product__price">
+          <span>€1300,00</span>
+          <span class="old-price">€1500,00</span>
+        </div>
+        <small>inkl. MwSt. zzgl. Versand</small>
+        <button type="button" class="h-button h-button__primary">Details</button>
+      </a>`;
 
-fetch("https://www.actec-klima.de/rest/categories", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+            document.querySelector('section .h-container').innerHTML += item;
+        });
+    }
+}
+if(name === 'Truhengeräte') {
+    menu3.map(menuItem => {
+        const item = `<a href="/product-detail.html?name=${menuItem.title.toLowerCase().replaceAll(' ','_')}" class="h-product">
+        <div class="h-product__img">
+          <img src="${menuItem.img}" alt="">
+        </div>
+        <h3>${menuItem.title}</h3>
+        <div class="h-product__price">
+          <span>€1300,00</span>
+          <span class="old-price">€1500,00</span>
+        </div>
+        <small>inkl. MwSt. zzgl. Versand</small>
+        <button type="button" class="h-button h-button__primary">Details</button>
+      </a>`;
+
+        document.querySelector('.h-products').innerHTML += item;
+    });
+    if(menu3.length < 1) {
+        document.querySelector('section .h-container').innerHTML += '<p class="kein-produkte">Es wurden keine Produkte gefunden, die deiner Auswahl entsprechen.</p>'
+    }
+}
